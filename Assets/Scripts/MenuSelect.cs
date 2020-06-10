e9462a3b50f85825bfebf2eb6e8ebb76b175f97b
Joaquin2 <joaquinebatista@gmail.com>
2020-06-09 22:19:24 -0700

    Added Unity Assets
    These are the Assets used in creating our Unity Application.
    
    You must also install the Windows Mixed Reality Toolkit and also turn on Vuforia. Make sure in player settings in XR settings you are set to multi-pass.

---
+++ Assets/Scripts/MenuSelect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSelect : MonoBehaviour
+{
+    public void ChangeMenuScene(string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
